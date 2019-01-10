5b6a5b9e280b95b0fe5d94c022ef0d5255404800
Josh Wolfe <joshuafwolfe@gmail.com>
2019-01-10 13:35:53 -0500

    Refactored out the admin stack and added a stack for school and minecraft server

--- docker/deploy-all.sh
+++ docker/deploy-all.sh
@@ -4,6 +4,8 @@ if ! docker network inspect webnet > /dev/null; then
     docker network create -d overlay webnet --attachable
 fi
 
-for dir in media registry vpn; do
-    $dir/deploy.sh
+for dir in admin registry vpn media; do
+    cd $dir
+    ./deploy.sh
+    cd ..
 done
