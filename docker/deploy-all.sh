1be6594f539a9adbfa6920a437ff44b05ad83a49
Anthony Tam <atam@DOCKER01.homeland.ad>
2020-05-01 15:21:48 -0400

    Added additional services

--- docker/deploy-all.sh
+++ docker/deploy-all.sh
@@ -4,7 +4,7 @@ if ! docker network inspect vmnet > /dev/null; then
     docker network create -d overlay vmnet --attachable
 fi
 
-for dir in admin vpn; do
+for dir in admin storage iot webapps; do
     cd $dir
     ./deploy.sh
     cd ..
