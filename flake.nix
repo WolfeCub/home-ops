23d1b4e20275c7d767055fd3173a8b53ccf3205a
Josh Wolfe <joshuafwolfe@gmail.com>
2026-07-06 15:29:53 -0400

    Bump up speaches model

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
+      systems = ["x86_64-linux" "aarch64-linux"];
 
       perSystem = {pkgs, ...}: let
         helm-with-secrets = pkgs.wrapHelm pkgs.kubernetes-helm {
